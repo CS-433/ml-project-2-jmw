87728e89613baee57bf0b7bc800718e2c41931a7
Wène <wene.kouarfate@epfl.ch>
2024-12-02 20:50:57 +0100

    annotations + merge_csv()

--- annotate.py
+++ annotate.py
@@ -7,6 +7,8 @@ import random
 import imageProcessing as ip
 import csv
 
+import pandas as pd
+
 
 """
 This is a simple script that allows a user to provide data by clicking on ant images.
@@ -81,13 +83,15 @@ def write_to_csv(data, output_file):
             ]
             writer.writerow(processed_row)
 
-
+def merge_csvs(*file_paths, final_path='.data.csv'):
+    df = pd.concat([ pd.read_csv(path) for path in file_paths ])
+    df.groupby(['Image Name']).mean().to_csv(final_path)
 
 if __name__ == "__main__":
 
     rel_path_source = input("Type the relative path of the folder containing the images you want to anotate: \n")
     rel_path_dest = input("Type the relative path of the destination file for the collected data: \n")
-    originals, image_names = ip.load_images_from_folder(rel_path_source, max_images=100)
+    originals, image_names = ip.load_images_from_folder(rel_path_source, max_images=198)
 
     """
     Could be a problem in the training data if image names aren't unique.
@@ -101,7 +105,7 @@ if __name__ == "__main__":
         if (i+1) % 5 == 0:
             write_to_csv(collected_data, rel_path_dest)
             collected_data = []
-            if (i+1) % 20 == 0:
+            if (i+1) % 100 == 0:
                 if input("Do you wish to continue ? Input 'y' to continue, 'n' to stop.") == "n":
                     break
 
